7e2efccb8b9b89a24010f67617179c08cb8f0431
sgomes2 <saulogomes3@gmail.com>
2024-12-06 16:45:27 -0500

    electron and IPC working

---
+++ index.js
@@ -0,0 +1,29 @@
+const { app, BrowserWindow, ipcMain } = require('electron')
+var net = require('net');
+
+function handlePixelSketchArray(event, data) {
+  var client = new net.Socket();
+  client.connect(80, 'pixelsketch.local', function () {
+    console.log('Arduino Connected');
+    client.write(data);
+    client.destroy();
+  });
+}
+
+const createWindow = () => {
+  const win = new BrowserWindow({
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false
+    },
+    width: 900,
+    height: 900
+  })
+
+  win.loadURL('http://localhost:3000')
+}
+
+app.whenReady().then(() => {
+  ipcMain.on('set-sketch', handlePixelSketchArray)
+  createWindow()
+})
--- src/App.js
+++ src/App.js
@@ -6,55 +6,56 @@ import Stack from '@mui/material/Stack';
 import Button from '@mui/material/Button';
 import DownloadIcon from '@mui/icons-material/Download';
 import UploadIcon from '@mui/icons-material/Upload';
+const { ipcRenderer } = window.require("electron");
 
-const io =  require("socket.io-client");
+// const io =  require("socket.io-client");
 
-const socket = io('http://192.168.1.160:9080');
+// const socket = io('http://192.168.1.160:9080');
 
-// Handle connection
-socket.on('connect', () => {
-    console.log('Connected to server');
-});
+// // Handle connection
+// socket.on('connect', () => {
+//     console.log('Connected to server');
+// });
 
 function App() {
   const [selectedColor, setSelectedColor] = useState(0)
   const [pixelGridValues, setPixelGridValues] = useState({});
   const updateLedArray = () => {
     let pixelArray = "";
-    for(let i = 0; i < 256; i++) {
-      pixelArray+=`[${i}:${pixelGridValues[i] || 0}]`
+    for (let i = 0; i < 256; i++) {
+      pixelArray += `[${i}:${pixelGridValues[i] || 0}]`
     }
     console.log(pixelArray);
 
     // Create WebSocket connection.
-    socket.emit('UPDATE', pixelArray);
+    ipcRenderer.send('set-sketch', pixelArray);
   }
-  
+
   return (
     <div className="App">
       <header className="App-header">
-        <h2 style={{marginTop: '10px'}}>Pixel Sketch</h2>
-        
-        <div style={{display:'flex', flexDirection:'row'}}>
-          <div style={{margin: '10px'}}>
-            <PixelGrid gridValues={pixelGridValues} updateGridValues={setPixelGridValues} selectedColor={selectedColor}/>
+        <h2 style={{ marginTop: '10px' }}>Pixel Sketch</h2>
+
+        <div style={{ display: 'flex', flexDirection: 'row' }}>
+          <div style={{ margin: '10px' }}>
+            <PixelGrid gridValues={pixelGridValues} updateGridValues={setPixelGridValues} selectedColor={selectedColor} />
           </div>
-          <div style={{margin: '10px'}}>
-            <ColorPicker onClick={setSelectedColor}/>
+          <div style={{ margin: '10px' }}>
+            <ColorPicker onClick={setSelectedColor} />
           </div>
         </div>
 
         <Stack direction="row" spacing={2}>
-      <Button variant="contained" startIcon={<DownloadIcon />}>
-        Download Sketch
-      </Button>
-      <Button variant="contained" endIcon={<UploadIcon />}>
-        Open Sketch
-      </Button>
-    </Stack>
-        
+          <Button variant="contained" startIcon={<DownloadIcon />}>
+            Download Sketch
+          </Button>
+          <Button variant="contained" endIcon={<UploadIcon />}>
+            Open Sketch
+          </Button>
+        </Stack>
+
         <button onClick={updateLedArray}>Light It Up!</button>
-        
+
       </header>
     </div>
   );
